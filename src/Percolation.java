33f3daf01d4f19e92058a3fed502b582520b99df
outzider <dgethers.jr@gmail.com>
2014-02-15 23:59:34 -0500

    Bug fix for false full sites on top row.

--- src/Percolation.java
+++ src/Percolation.java
@@ -17,20 +17,6 @@ public class Percolation {
         isFullWeightedQuickUnionUF = new WeightedQuickUnionUF((N * N) + 1);
         open = new boolean[N][N];
 
-        if (N > 1) {
-            //join top row with source point
-            for (int i = 0; i < N; i++) {
-                int q = xyTo1D(0, i);
-                weightedQuickUnionUF.union(0, q);
-                isFullWeightedQuickUnionUF.union(0, q);
-            }
-
-            //join bottom row with sink point
-            for (int i = 0; i < N; i++) {
-                int q = xyTo1D(N - 1, i);
-                weightedQuickUnionUF.union(N * N + 1, q);
-            }
-        }
     }
 
     // open site (row i, column j) if it is not already
@@ -61,14 +47,19 @@ public class Percolation {
             isFullWeightedQuickUnionUF.union(xyTo1D((i - 1), (j - 1)), xyTo1D((i - 1) + 1, (j - 1)));
         }
 
-        if (N == 1) { //only one element so link source and sink
-            weightedQuickUnionUF.union(0, xyTo1D((i - 1), (j - 1)));
-            weightedQuickUnionUF.union(xyTo1D((i - 1), (j - 1)), N * N + 1);
+        //if open item is in top row join to source point
+        if ((i - 1) == 0) {
+            weightedQuickUnionUF.union(0, xyTo1D(i - 1, j - 1));
+            isFullWeightedQuickUnionUF.union(0, xyTo1D(i - 1, j - 1));
+        }
+
+        //if open item is in bottom row join to sink point
+        if ((i - 1) == (N - 1)) {
+            weightedQuickUnionUF.union(N * N + 1, xyTo1D(i - 1, j - 1));
         }
 
         open[(i - 1)][(j - 1)] = true;
     }
-
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
@@ -94,11 +85,11 @@ public class Percolation {
     }
 
     private void checkInputParameters(int i, int j) {
-        if (i < 0 || i > N) {
+        if (i <= 0 || i > N) {
             throw new IndexOutOfBoundsException();
         }
 
-        if (j < 0 || j > N) {
+        if (j <= 0 || j > N) {
             throw new IndexOutOfBoundsException();
         }
     }
