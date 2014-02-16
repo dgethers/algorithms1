c6dc0843e0361ff1b6de44cdf4fc45a477293823
outzider <dgethers.jr@gmail.com>
2014-02-16 00:10:11 -0500

    Throw IllegalArgumentException for inputs less than or equal to zero.

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -10,6 +10,11 @@ public class PercolationStats {
 
     // perform T independent computational experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
+
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+
         percentOfSitesOpen = new double[T];
         this.T = T;
 
