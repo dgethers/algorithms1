4b01129d08c59a366fa58378f4b42a42a9771725
outzider <dgethers.jr@gmail.com>
2014-02-21 23:11:12 -0500

    Initial revision for Collinear.

---
+++ src/Brute.java
@@ -0,0 +1,11 @@
+/**
+ * User: outzider
+ * Date: 2/21/14
+ * Time: 11:07 PM
+ */
+public class Brute {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Fast.java
@@ -0,0 +1,11 @@
+/**
+ * User: outzider
+ * Date: 2/21/14
+ * Time: 11:07 PM
+ */
+public class Fast {
+
+    public static void main(String[] args) {
+
+    }
+}
