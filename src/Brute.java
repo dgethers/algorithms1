6b8a8d6b59c6e56af006c33db84ed3373db25ca7
outzider <dgethers.jr@gmail.com>
2014-03-01 14:04:34 -0500

    Style recommendation.

--- src/Brute.java
+++ src/Brute.java
@@ -19,8 +19,8 @@ public class Brute {
             for (int j = i + 1; j < coordinates.length; j++) {
                 for (int k = j + 1; k < coordinates.length; k++) {
                     for (int l = k + 1; l < coordinates.length; l++) {
-                        if (coordinates[i].slopeTo(coordinates[j]) == coordinates[i].slopeTo(coordinates[k]) &&
-                                coordinates[i].slopeTo(coordinates[k]) == coordinates[i].slopeTo(coordinates[l])) {
+                        if (coordinates[i].slopeTo(coordinates[j]) == coordinates[i].slopeTo(coordinates[k])
+                                && coordinates[i].slopeTo(coordinates[k]) == coordinates[i].slopeTo(coordinates[l])) {
                             Point[] tmp = {coordinates[i], coordinates[j], coordinates[k], coordinates[l]};
                             Arrays.sort(tmp);
                             System.out.printf("%s -> %s -> %s -> %s%n", tmp[0], tmp[1], tmp[2], tmp[3]);
