219f94c5610408f3c6924946d68aa5ae751bc7f7
dgethers <dgethers.jr@gmail.com>
2014-03-11 11:43:07 -0400

    Initial

---
+++ src/Board.java
@@ -0,0 +1,72 @@
+/**
+ * User: outzider
+ * Date: 3/11/14
+ * Time: 10:49 AM
+ */
+
+public class Board {
+
+    // construct a board from an N-by-N array of blocks
+    public Board(int[][] blocks) {
+        //TODO: Implement this
+    }
+
+    // (where blocks[i][j] = block in row i, column j)
+    // board dimension N
+    public int dimension() {
+        //TODO: Implement this
+
+        return 0;
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+        //TODO: Implement this
+
+        return 0;
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        //TODO: Implement this
+
+        return 0;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        //TODO: Implement this
+
+        return false;
+    }
+
+    // a board obtained by exchanging two adjacent blocks in the same row
+    public Board twin() {
+        //TODO: Implement this
+
+        return null;
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+        //TODO: Implement this
+
+        return false;
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+        //TODO: Implement this
+
+        return null;
+    }
+
+    // string representation of the board (in the output format specified below)
+    public String toString() {
+
+        //TODO: Implement this
+        return null;
+    }
+}
+
+
