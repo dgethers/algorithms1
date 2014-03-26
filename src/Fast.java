bdaf86848d8256fe17211ba4c3c42e68db1539ef
DGethers <david.gethers@jcrew.com>
2014-03-26 14:17:27 -0400

    FINALLY a working solution with no sub-segments printed or drawn.

--- src/Fast.java
+++ src/Fast.java
@@ -9,10 +9,9 @@ public class Fast {
 
     private Point[] coordinates;
 
-    //TODO: Uncomment calls to draw points and lines
     public Fast() {
-//        StdDraw.setXscale(0, 32768);
-//        StdDraw.setYscale(0, 32768);
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
     }
 
     private void setPointsFromInputFile(String inputFile) {
@@ -22,81 +21,81 @@ public class Fast {
         for (int i = 0; i < coordinates.length; i++) {
             Point point = new Point(in.readInt(), in.readInt());
             coordinates[i] = point;
-//            point.draw();
+            point.draw();
         }
     }
 
     private void printAndDrawCollinearLineSegments() {
-        int absMinIndex = -1;
-        int absMaxIndex = -1;
-        Point absMinPoint = null;
-        boolean firstMatchFound = true;
-        for (int i = 0; i < coordinates.length; i++) {
-//            System.out.printf("Currently on %d iteration%n", i);
-            Point p = coordinates[i];
-            if (absMinPoint == null) {
-                absMinPoint = p;
-            }
-            Point[] tmp = Arrays.copyOfRange(coordinates, i, coordinates.length);
-            Arrays.sort(tmp);
+        Arrays.sort(coordinates);
+        Point[] tmp = Arrays.copyOfRange(coordinates, 0, coordinates.length);
+        for (Point p : coordinates) {
             Arrays.sort(tmp, p.SLOPE_ORDER);
-            int minIndex = -1;
-            int maxIndex = -1;
             double slopeTo = Double.NaN;
-
+            int min = -1;
+            int max = -1;
+            int matchCount = 1;
             for (int j = 0; j < tmp.length; j++) {
                 Point q = tmp[j];
-//                System.out.printf("Index: %d | Working on %s & %s with slope %f%n", j, p, q, p.slopeTo(q));
+//                System.out.printf("Comparing %s with %s with slope %f and compare %d%n", p, q, p.slopeTo(q), p.compareTo(q));
 
-                if (p.slopeTo(q) == slopeTo) {
-                    if (minIndex == -1) {
-                        minIndex = j - 1;
-
-                        if (absMinIndex == -1 || absMinIndex > minIndex) {
-                            absMinIndex = i;
-                            absMinPoint = tmp[absMinIndex];
-                        }
+                if (slopeTo == p.slopeTo(q)) { //slope is the same and is not the origin point
+                    if (min == -1) {
+                        min = j;
                     }
 
-                    maxIndex = j;
+                    max = j;
+                    matchCount++;
+                }
 
-                    if (absMaxIndex < maxIndex) {
-                        absMaxIndex = maxIndex;
+                if (slopeTo != p.slopeTo(q)) {
+                    if (matchCount > 2 && pIsLessThanAllOfItsPart(tmp, p, min, max)) {
+//                        System.out.printf("Segment from %s with matchCount of %d: %s with slope %f%n", p, matchCount, Arrays.toString(Arrays.copyOfRange(tmp, min - 1, max + 1)), slopeTo);
+                        printAndDrawLineSegment(tmp, p, min, max, matchCount);
                     }
+                    min = -1;
+                    matchCount = 1;
                 }
 
                 slopeTo = p.slopeTo(q);
             }
 
-//            System.out.printf("minIndex: %d, maxIndex: %d | absMinIndex: %d, absMaxIndex: %d%n", minIndex, maxIndex, absMinIndex, absMaxIndex);
-
-            if (minIndex > -1) {
-//                Point[] results = Arrays.copyOfRange(tmp, minIndex, maxIndex + 1);
-                Point[] results = new Point[maxIndex - minIndex + 2];
-                results[0] = tmp[0];
-                int index = 1;
-                for (int k = minIndex; k <= maxIndex; k++) {
-                    results[index++] = tmp[k];
-                }
-                Arrays.sort(results);
-//                System.out.printf("size of result array: %d%n", results.length);
-                if (results.length > 3) {
-                    boolean isWithinMinMaxRange = minIndex >= absMinIndex && maxIndex < absMaxIndex;
-                    boolean isFirstElementTheSameAsAbsoluteFirst = results[0].compareTo(absMinPoint) == 0;
-//                    System.out.println("results[0] = " + results[0]);
-//                    System.out.println("absMinPoint = " + absMinPoint);
-//                    System.out.println("isWithinMinMaxRange = " + isWithinMinMaxRange);
-//                    System.out.println("isFirstElementTheSameAsAbsoluteFirst = " + isFirstElementTheSameAsAbsoluteFirst);
-//                    System.out.println("firstMatchFound = " + firstMatchFound);
-                    if (!isWithinMinMaxRange || (!isFirstElementTheSameAsAbsoluteFirst)) {
-                        System.out.printf("Result: %s%n", Arrays.toString(results));
-                    }
-
-                }
-                firstMatchFound = false;
-                absMinPoint = results[0];
+            if (matchCount > 2 && pIsLessThanAllOfItsPart(tmp, p, min, max)) {
+//                System.out.printf("Segment from %s with matchCount of %d: %s with slope %f%n", p, matchCount, Arrays.toString(Arrays.copyOfRange(tmp, min - 1, max + 1)), slopeTo);
+                printAndDrawLineSegment(tmp, p, min, max, matchCount);
             }
         }
+
+//        System.out.println("segmentCount = " + segmentCount);
+    }
+
+    private boolean pIsLessThanAllOfItsPart(Point[] array, Point p, int min, int max) {
+        for (int i = min - 1; i <= max; i++) {
+            if (p.compareTo(array[i]) != -1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void printAndDrawLineSegment(Point[] array, Point p, int min, int max, int matchCount) {
+        int index = 0;
+        Point[] result = new Point[matchCount + 1];
+        result[index++] = p;
+        for (int i = min - 1; i <= max; i++) {
+            result[index++] = array[i];
+        }
+        Arrays.sort(result);
+
+        for (int i = 0; i < result.length; i++) {
+            StdOut.print(result[i]);
+
+            if (i < result.length - 1) {
+                StdOut.print(" -> ");
+            }
+        }
+        StdOut.println();
+        result[0].drawTo(result[result.length - 1]);
     }
 
     public static void main(String[] args) {
